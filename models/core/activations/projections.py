9aa9565b4ccbb7aaa76416f92af0d6f9882dbdd6
krishpranav <krisna.pranav@gmail.com>
2026-06-27 18:19:21 +0530

    models: core[activations(projections)]

---
+++ models/core/activations/projections.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+from torch import Tensor, embedding
+from torch import nn
+from models.shared.layers.linear import Linear
+
+class QKVProjection(nn.Module):
+    def __init__(
+        self,
+        embedding_dim: int,
+        num_heads: int,
+    ) -> None:
+        super().__init__()
+
+        if embedding_dim % num_heads != 0:
+            raise ValueError(
+                "embedding_dm must be divisible"
+                "by num_heads."
+            )
+
+        self.embedding_dm = embedding_dim
+        self.num_heads = num_heads
+
+        self.head_dim = (
+            embedding_dim // num_heads
+        )
+
+        self.qkv_projection = Linear(
+            embedding_dim,
+            embedding_dim * 3,
+        )
+
+    def forward(
+        self,
+        inputs: Tensor,
+    ) -> tuple[Tensor, Tensor, Tensor]:
+        if inputs.ndim != 3:
+            raise ValueError(
+                "Expected tensor with shape"
+                "[B, N, D]."
+            )
+        
+        batch_size, sequence_length, _ = (
+            inputs.shape
+        )
+
+        qkv = self.qkv_projection(inputs)
+
+        
