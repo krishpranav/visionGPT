c8d7bb3d4f4e7ae5c389c0177f872cb7daadb747
krishpranav <krisna.pranav@gmail.com>
2026-06-27 18:19:51 +0530

    models: core[activations(projections)]

--- models/core/activations/projections.py
+++ models/core/activations/projections.py
@@ -1,9 +1,29 @@
 from __future__ import annotations
-from torch import Tensor, embedding
+from torch import Tensor
 from torch import nn
 from models.shared.layers.linear import Linear
 
+
 class QKVProjection(nn.Module):
+    """
+    Projects token embeddings into query, key,
+    and value representations.
+
+    Input:
+
+        [B, N, D]
+
+    Output:
+
+        Query
+        Key
+        Value
+
+    Each output shape:
+
+        [B, H, N, Dh]
+    """
+
     def __init__(
         self,
         embedding_dim: int,
@@ -13,11 +33,11 @@ class QKVProjection(nn.Module):
 
         if embedding_dim % num_heads != 0:
             raise ValueError(
-                "embedding_dm must be divisible"
+                "embedding_dim must be divisible "
                 "by num_heads."
             )
 
-        self.embedding_dm = embedding_dim
+        self.embedding_dim = embedding_dim
         self.num_heads = num_heads
 
         self.head_dim = (
@@ -35,14 +55,40 @@ class QKVProjection(nn.Module):
     ) -> tuple[Tensor, Tensor, Tensor]:
         if inputs.ndim != 3:
             raise ValueError(
-                "Expected tensor with shape"
+                "Expected tensor with shape "
                 "[B, N, D]."
             )
-        
+
         batch_size, sequence_length, _ = (
             inputs.shape
         )
 
         qkv = self.qkv_projection(inputs)
 
-        
+        qkv = qkv.view(
+            batch_size,
+            sequence_length,
+            3,
+            self.num_heads,
+            self.head_dim,
+        )
+
+        qkv = qkv.permute(
+            2,
+            0,
+            3,
+            1,
+            4,
+        )
+
+        queries = qkv[0]
+
+        keys = qkv[1]
+
+        values = qkv[2]
+
+        return (
+            queries,
+            keys,
+            values,
+        )
