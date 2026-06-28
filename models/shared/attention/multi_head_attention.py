a1d3ff8cd6e46a1eb51c1fc79581434a0270604e
krishpranav <krisna.pranav@gmail.com>
2026-06-28 13:13:32 +0530

    models: shared[attention(multi_head_attention)]

--- models/shared/attention/multi_head_attention.py
+++ models/shared/attention/multi_head_attention.py
@@ -1,23 +1,54 @@
 from __future__ import annotations
+
 from torch import Tensor
 from torch import nn
+
 from models.shared.attention.qkv_projection import (
     QKVProjection,
 )
-
 from models.shared.attention.scaled_dot_product import (
     ScaledDotProductAttention,
 )
-
 from models.shared.layers.linear import Linear
 
+
 class MultiHeadAttention(nn.Module):
+    """
+    VisionGPT Multi-Head Self-Attention.
+
+    Architecture
+
+        Input
+          │
+          ▼
+      QKV Projection
+          │
+          ▼
+    Scaled Dot Product
+          │
+          ▼
+     Output Projection
+          │
+          ▼
+         Output
+
+    Input
+    -----
+
+        [B, N, D]
+
+    Output
+    ------
+
+        [B, N, D]
+    """
+
     def __init__(
         self,
         embedding_dim: int,
         num_heads: int,
         *,
-        attention_doprout: float = 0.0,
+        attention_dropout: float = 0.0,
         projection_dropout: float = 0.0,
     ) -> None:
         super().__init__()
@@ -34,7 +65,7 @@ class MultiHeadAttention(nn.Module):
 
         if embedding_dim % num_heads != 0:
             raise ValueError(
-                "embedding_dim must be divisble by zero."
+                "embedding_dim must be divisible by "
                 "num_heads."
             )
 
@@ -42,17 +73,17 @@ class MultiHeadAttention(nn.Module):
         self.num_heads = num_heads
 
         self.qkv_projection = QKVProjection(
-            embedding_dim = embedding_dim,
-            num_heads = num_heads
+            embedding_dim=embedding_dim,
+            num_heads=num_heads,
         )
 
         self.attention = ScaledDotProductAttention(
-            attention_doprout = attention_dropout,
+            attention_dropout=attention_dropout,
         )
 
         self.output_projection = Linear(
             embedding_dim,
-            embedding_dim
+            embedding_dim,
         )
 
         self.output_dropout = nn.Dropout(
@@ -65,36 +96,69 @@ class MultiHeadAttention(nn.Module):
         *,
         attention_mask: Tensor | None = None,
     ) -> Tensor:
+        """
+        Parameters
+        ----------
+        inputs
+
+            Shape:
+
+                [B, N, D]
+
+        Returns
+        -------
+
+        Tensor
+
+            Shape:
+
+                [B, N, D]
+        """
+
         if inputs.ndim != 3:
             raise ValueError(
                 "Expected tensor with shape "
-                "[B, N, D]"
+                "[B, N, D]."
             )
 
         batch_size, sequence_length, _ = (
             inputs.shape
         )
 
-        queries, keys, value = (
+        queries, keys, values = (
             self.qkv_projection(inputs)
         )
 
         outputs = self.attention(
-            queries = queries,
-            keys = keys,
-            values = values,
-            attention_mask = attention_mask,
+            queries=queries,
+            keys=keys,
+            values=values,
+            attention_mask=attention_mask,
         )
 
         outputs = outputs.transpose(
             1,
-            2
+            2,
+        )
+
+        outputs = outputs.reshape(
+            batch_size,
+            sequence_length,
+            self.embedding_dim,
+        )
+
+        outputs = self.output_projection(
+            outputs,
+        )
+
+        outputs = self.output_dropout(
+            outputs,
         )
 
         return outputs
 
     def extra_repr(self) -> str:
         return (
-            f"embedding_dim = {self.embedding_dim}, "
-            f"num_heads = {self.num_heads}"  
+            f"embedding_dim={self.embedding_dim}, "
+            f"num_heads={self.num_heads}"
         )
